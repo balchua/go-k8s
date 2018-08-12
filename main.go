29eb4e666e35ce3c3140c876f7fdda0cfe29dca0
balchua@yahoo.com <balchua@yahoo.com>
2018-08-12 10:31:44 +0800

    added logrus text formatter with proper timestamp

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
@@ -36,6 +19,19 @@ var namespace string
 var pathToConfig string
 
 const ctmJobAnnotation string = "sample.com/job-orchestrator"
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true})
+
+	// Output to stdout instead of the default stderr, could also be a file.
+	logrus.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.DebugLevel)
+
+}
 
 func main() {
 	app := cli.NewApp()
@@ -64,8 +60,9 @@ func main() {
 		go pollServices()
 		// watchNodes()
 		for {
+			//keep the application alive.
 			time.Sleep(5 * time.Second)
-			logrus.Infof("Doing nothing")
+			logrus.Debug("Doing nothing")
 		}
 	}
 	app.Run(os.Args)
